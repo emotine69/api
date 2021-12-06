6003c4f191c0e83a8c58e5d10b77bb47a73308ff
mrpapdiamond <mrpapdiamond@gmail.com>
2021-12-06 21:43:47 +0700

    main

---
+++ backend/api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework import serializers
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Products
+from .serializers import ProductsSerializer
+# Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls={
+        'Products':'/products/',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def ProductsList(request):
+    products = Products.objects.all()
+    serializer = ProductsSerializer(products,many=True)
+    return Response(serializer.data)
